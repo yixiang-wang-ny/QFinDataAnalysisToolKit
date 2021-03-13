6d2cf44fb3db71480a0fbf657454e0f2c8b6b5d9
yixiang-wang-ny <yixiang.wang.ny@gmail.com>
2021-03-13 11:43:39 -0500

    update

--- data/data_layer.py
+++ data/data_layer.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import pandas as pd
+from data.feature import FeatureOnPandasDataFrame
 
 
 class Data(object):
@@ -9,8 +10,14 @@ class Data(object):
         if data_id is None:
             self.data_id = int(dt.datetime.now().strftime('%Y%m%d%H%M%S'))
 
+        self.feature_map = {}
+
     def add_from_csv(self, csv_path):
 
         df = pd.read_csv(csv_path)
-        return
+        for col in df:
+            feature = FeatureOnPandasDataFrame(col, df)
+
+            self.feature_map[feature.name] = feature
+
 
--- data/feature.py
+++ data/feature.py
@@ -1,5 +1,47 @@
-class FeatureTemplate(object):
+from abc import ABC, abstractmethod
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
 
-    def __init__(self, is_numeric, has_missing_value, is_factor):
 
-        pass
+class FeatureTemplate(ABC):
+
+    name = None
+
+    @abstractmethod
+    def get_data(self):
+        return
+
+    @abstractmethod
+    def is_numeric(self):
+        return
+
+    @abstractmethod
+    def is_factor(self):
+        return
+
+    @abstractmethod
+    def has_missing_value(self):
+        return
+
+
+class FeatureOnPandasDataFrame(FeatureTemplate):
+
+    def __init__(self, name, underlying_data_frame):
+
+        self.name = name
+        self.underlying_data_frame = underlying_data_frame
+        self._has_missing = self.underlying_data_frame[self.name].isnull().any()
+        self._is_numeric = is_numeric_dtype(self.underlying_data_frame[self.name])
+        self._is_factor = not self._is_numeric
+
+    def get_data(self):
+        return self.underlying_data_frame[self.name]
+
+    def is_numeric(self):
+        return self._is_numeric
+
+    def is_factor(self):
+        return self._is_factor
+
+    def has_missing_value(self):
+        return self._has_missing
