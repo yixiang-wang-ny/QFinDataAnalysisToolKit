26160c9a85822e3bae542e8a870ff16be961687f
yixiang-wang-ny <yixiang.wang.ny@gmail.com>
2021-03-21 13:25:54 -0400

    Update criteria.py

--- model/criteria.py
+++ model/criteria.py
@@ -20,8 +20,8 @@ class DirectionalAccuracy(PerformanceMeasure):
 
     def score(self, predicted: np.array, true_response: np.array):
 
-        predict_zero_one_out = np.array([0 if x < 0 else 1 for x in predicted])
-        true_zero_one_out = np.array([0 if x < 0 else 1 for x in true_response])
+        predict_zero_one_out = np.array([0 if x <= 0 else 1 for x in predicted])
+        true_zero_one_out = np.array([0 if x <= 0 else 1 for x in true_response])
 
         return sum(predict_zero_one_out == true_zero_one_out) / float(len(predict_zero_one_out))
 
